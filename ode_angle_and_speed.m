948ff4a3f2ac46c04808c8be15046ac1dd5f4d01
Olivia Chang <ochang@olin.edu>
2021-12-05 10:02:50 -0500

    forgot to use global variables in ode_angle_and_speed.m

--- ode_angle_and_speed.m
+++ ode_angle_and_speed.m
@@ -1,9 +1,8 @@
 function [found_angle, found_speed, error] = ode_angle_and_speed(desired_time)
     %Finding the angle and speed where the difference between desired time 
     %and measured time is zero
-    x_distance = 1;
-    y_distance = 0;
-    [initial_speed_guess, angle_guess] = get_speed_and_angle(desired_time,x_distance,y_distance);
+    global initial_height_launcher x_distance_to_piano y_distance_to_piano;
+    [initial_speed_guess, angle_guess] = get_speed_and_angle(desired_time,x_distance_to_piano,y_distance_to_piano);
     [found_angle, error] = fminsearch(@(a) time_error(desired_time, initial_speed_guess, a), angle_guess);
     found_speed = find_speed0(found_angle,100); % input degrees, get speed
 end
