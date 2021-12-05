10a1cc3f7fa635d5bffad24b03d409fe7314e453
Olivia Chang <ochang@olin.edu>
2021-12-04 21:46:05 -0500

    clean up code a bit

--- find_closest_angle.m
+++
@@ -1,8 +0,0 @@
-function [closest_angle, error] = find_closest_angle(desired_time)
-    % find angle that minimizes the time error for desired time
-    x_distance = 1;
-    y_distance = 0;
-    [initial_speed_guess, angle_guess] = get_speed_and_angle(desired_time,x_distance,y_distance);
-
-    [closest_angle, error] = fminsearch(@(a) time_error(desired_time, initial_speed_guess, a), angle_guess);
-end
